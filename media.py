0f35dc90f8be957570d45908f01d5a5f2cc82d5a
alexhn <alex@alexn.ee>
2018-04-12 20:17:05 +0300

    Add class comment

--- media.py
+++ media.py
@@ -1,5 +1,23 @@
+####################################################################################
+# Project: Movie website
+# ##################################################################################
+
+#################################### Media.py ######################################
+# Description: This file defines class Movie to allow instances of this class to be
+# used in th entertaiment.py file.
+####################################################################################
+
 class Movie:
+    """ The Movie class represents single movie on our website """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """
+        Creates an instance of Movie class with specified title,
+        poster url and YouTube trailer url.
+
+        :param title: the title of the Movie
+        :param poster_image_url: poster image URL
+        :param trailer_youtube_url: URL to movie trailer on YouTube
+        """
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
